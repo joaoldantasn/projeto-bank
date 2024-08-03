f0079945abb63fd268b3efa06638c730efc2e74f
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-08-03 10:36:51 -0300

    Usuario & Agencia

---
+++ ebank/src/main/java/com/br/accenture/eBank/ebank/entities/Agencia.java
@@ -0,0 +1,75 @@
+package com.br.accenture.eBank.ebank.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_agencia")
+public class Agencia {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idAgencia;
+	private int codAgencia;
+	private String endereco;
+	private String telefone;
+	
+	@OneToMany(mappedBy = "agencia")
+	private Set<Usuario>usuarios = new HashSet<>();
+	
+	public Agencia() {
+		
+	}
+
+	public Agencia(Long idAgencia, int codAgencia, String endereco, String telefone) {
+		this.idAgencia = idAgencia;
+		this.codAgencia = codAgencia;
+		this.endereco = endereco;
+		this.telefone = telefone;
+	}
+
+	public Long getIdAgencia() {
+		return idAgencia;
+	}
+
+	public void setIdAgencia(Long idAgencia) {
+		this.idAgencia = idAgencia;
+	}
+
+	public int getCodAgencia() {
+		return codAgencia;
+	}
+
+	public void setCodAgencia(int codAgencia) {
+		this.codAgencia = codAgencia;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Set<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	
+}
---
+++ ebank/src/main/java/com/br/accenture/eBank/ebank/entities/Usuario.java
@@ -0,0 +1,102 @@
+package com.br.accenture.eBank.ebank.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idUsuario;
+	private String cpf;
+	private String nomeUsuario;
+	private String telefone;
+	private String senha;
+	
+	@ManyToOne
+	@JoinColumn(name = "agencia_id")
+	private Agencia agencia;
+	
+	public Usuario() {
+		
+	}
+	
+
+	public Usuario(Long idUsuario, String cpf, String nomeUsuario, String telefone, String senha, Agencia agencia) {
+		this.idUsuario = idUsuario;
+		this.cpf = cpf;
+		this.nomeUsuario = nomeUsuario;
+		this.telefone = telefone;
+		this.senha = senha;
+		this.agencia = agencia;
+	}
+
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+	public Agencia getAgencia() {
+		return agencia;
+	}
+
+
+	public void setAgencia(Agencia agencia) {
+		this.agencia = agencia;
+	}
+
+	
+}
