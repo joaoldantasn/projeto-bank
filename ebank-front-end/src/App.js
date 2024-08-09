73b2585c2ad736f738d35c3c76aa661fc65589d6
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2024-08-08 23:26:05 -0300

    Novas dependências adicionadas  e mudança na versão do node

--- ebank-front-end/src/App.js
+++ ebank-front-end/src/App.js
@@ -1,9 +1,22 @@
+import './global.css';
+import { PrimeReactCSS } from "primereact/resources/themes/saga-green/theme.css";
+
+//import "primereact/resources/themes/lara-light-blue/theme.css";
+//import "primereact/resources/themes/lara-dark-blue/theme.css";
+//import "primereact/resources/themes/saga-blue/theme.css";
+//import "primereact/resources/themes/saga-orange/theme.css";
+
+import { PrimeReactMinCsss } from "primereact/resources/primereact.min.css";
+import { Icons } from "primeicons/primeicons.css";
+import 'primeflex/primeflex.css';
+import axios from 'axios';
+import Cliente from './Pages/Cliente';
+
 
 function App() {
   return (
     <div>
-      <h1>ebank-front-end $ </h1>
-      <h3>Accenture Best Bank</h3>
+      <Cliente></Cliente>
     </div>
   );
 }
