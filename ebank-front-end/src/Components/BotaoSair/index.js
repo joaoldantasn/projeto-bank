9a32594b893cfde39e0b2167454e0d150a94beb6
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2024-08-09 22:17:08 -0300

    create BotaoSair

---
+++ ebank-front-end/src/Components/BotaoSair/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
+export default function BotaoSair() {
+
+    const history = useHistory();
+    var configBotao = "p-mb-3";
+    var largura = window.screen.width;
+    if (largura < 640) {
+        configBotao = "p-mt-3 ";
+    }
+    const encerrarSessao = () => {
+        localStorage.clear();
+        history.push('/');
+    }
+
+    return (
+        <Button className={configBotao} label="SAIR" onClick={encerrarSessao} />
+    );
+}
