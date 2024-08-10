d84640fc14e51fd8678158188faec22af6ab798b
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2024-08-09 22:23:52 -0300

    create PainelTransferencia

---
+++ ebank-front-end/src/Components/PainelTransferencia/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+
+export default function PainelTransferencia() {
+
+    const [senha, setSenha] = useState('');
+
+    return (
+        <Card >
+            <Card subTitle='VALOR' >
+                <InputText
+                    // className={nomeObrigatorio}
+                    style={{ width: '25%' }}
+                // value={nome}
+                // onChange={(e) => setNome(e.target.value)}
+                />
+            </Card>
+
+            <Card subTitle='CONTA DE DESTINO' >
+                <InputText
+                    // className={nomeObrigatorio}
+                    style={{ width: '25%' }}
+                // value={nome}
+                // onChange={(e) => setNome(e.target.value)}
+                />
+            </Card>
+
+            <Card subTitle='SENHA' >
+                <div className="card flex justify-content-center">
+                    <Password value={senha}
+                        onChange={(e) => setSenha(e.target.value)} toggleMask />
+                </div>
+            </Card>
+            <Card>
+                <Button className={''} label="ENVIAR" onClick={null} />
+            </Card>
+
+        </Card>
+
+    );
+}
