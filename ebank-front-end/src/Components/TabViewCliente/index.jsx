0a2a21dff31efba5f7246acf490c19670349eede
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2024-08-09 22:24:37 -0300

    create TabViwerCliente

---
+++ ebank-front-end/src/Components/TabViewCliente/index.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Card } from 'primereact/card';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { Toast } from 'primereact/toast';
+import { TabView, TabPanel } from 'primereact/tabview';
+import PainelDeposito from '../PainelDeposito';
+import PainelTransferencia from '../PainelTransferencia';
+import PainelSaque from '../PainelSaque';
+import PainelDataExtrato from '../PainelDataExtrato';
+import AppToolbar from '../AppToolbar';
+import SaldoToolbar from '../SaldoToolbar';
+import NomeToolbar from '../NomeToolbar';
+
+export default function TabViewCliente(props) {
+
+    const toast = useRef(null);
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    return (
+        <div>
+            <Toast ref={toast} />
+            <AppToolbar></AppToolbar>
+            <NomeToolbar></NomeToolbar>
+            <SaldoToolbar></SaldoToolbar>
+            <Card>
+                <div>
+                    <TabView activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}>
+
+                        <TabPanel header="SAQUE">
+                            <PainelSaque></PainelSaque>
+                        </TabPanel>
+                        <TabPanel header="DEPOSITO">
+                            <PainelDeposito></PainelDeposito>
+                        </TabPanel>
+                        <TabPanel header="EXTRATO">
+                            <PainelDataExtrato></PainelDataExtrato>
+                        </TabPanel>
+                        <TabPanel header="TRANSFERÊNCIA">
+                            <PainelTransferencia></PainelTransferencia>
+                        </TabPanel>
+
+                    </TabView>
+                </div>
+            </Card>
+        </div>
+    );
+}
