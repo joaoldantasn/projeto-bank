af16cc184c9a855252059642d174a0a3d7b296a1
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2024-08-11 01:18:13 -0300

    update PainelSaque
    ajuste dos componentes e adição de mensagem toast.

--- ebank-front-end/src/Components/PainelSaque/index.jsx
+++ ebank-front-end/src/Components/PainelSaque/index.jsx
@@ -1,22 +1,42 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from 'react';
 import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
+import { Toast } from 'primereact/toast';
+import TrasacaoService from '../../Services/TrasacaoService';
 
 export default function PainelSaque() {
 
     const [senha, setSenha] = useState('');
+    const [saque, setSaque] = useState('');
+
+    const toast = useRef(null);
+    const showSuccess = () => {
+        toast.current.show({
+            severity: 'success',
+            summary: 'Enviado com Sucesso!',
+            detail: 'Obrigado',
+            life: 5000
+        });
+    }
+
+    async function submeter() {
+        const contaID = 1
+        TrasacaoService.postSacar(contaID, saque)
+        showSuccess();
+    }
 
     return (
         <Card >
+            <Toast ref={toast} />
             <Card subTitle='VALOR' >
                 <InputText
                     // className={nomeObrigatorio}
                     style={{ width: '25%' }}
-                // value={nome}
-                // onChange={(e) => setNome(e.target.value)}
+                    value={saque}
+                    onChange={(e) => setSaque(e.target.value)}
                 />
             </Card>
             <Card subTitle='SENHA' >
@@ -27,7 +47,7 @@ export default function PainelSaque() {
                 </div>
             </Card>
             <Card>
-                <Button className={''} label="ENVIAR" onClick={null} />
+                <Button className={''} label="ENVIAR" onClick={submeter} />
             </Card>
 
         </Card>
