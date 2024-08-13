abef5a9c58c95dde4c5856360fd2b018de1cf373
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2024-08-13 12:35:37 -0300

    Update AuthService.js

--- ebank-front-end/src/Services/AuthService.js
+++ ebank-front-end/src/Services/AuthService.js
@@ -1,23 +1,13 @@
 import axios from 'axios';
 
-const headers = {
-    'headers': {
-        //'Authorization': 'Bearer ' + localStorage.getItem('token'),
-        'Content-Type': 'application/json'
-    }
-}
-
 class AuthService {
-
     postLogin(auth) {
-
-        return axios.post('http://localhost:8080/auth/login', auth, headers)
-            .then(Response => {
-                const token = Response.data.token
-                console.log(token)
-                localStorage.setItem('token', token)
+        return axios.post('http://localhost:8080/auth/login', auth)
+            .then(response => {
+                const token = response.data.token;
+                localStorage.setItem('token', token);
             })
-            .catch(error => console.log(error))
+            .catch(error => console.log(error));
     }
 
 }
